8aa4aa193c6f418f1dada657fc64dbae766cf798
Ilya <greenmine94@gmail.com>
2024-02-08 13:51:25 +0300

    feat(pos): add AppealListRequest

--- src/pos/mod.rs
+++ src/pos/mod.rs
@@ -10,30 +10,29 @@ use crate::service::{Message, Service};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-pub struct Pos {
-    i: AtomicUsize,
-}
+pub struct Pos {}
 
 impl Pos {
     pub const fn new() -> Self {
-        Self {
-            i: AtomicUsize::new(0),
-        }
-    }
-
-    pub fn next(&self) -> usize {
-        self.i.fetch_add(1, Ordering::Relaxed)
+        Self {}
     }
 }
 
-// TODO: error was: can't leak private type
 #[derive(Debug, Deserialize, Clone)]
-pub struct PosRequest {
-    #[serde(rename = "@one")]
-    one: String,
+pub struct PosEdmsRequest {
+    #[serde(rename = "$value")]
+    request: PosEdmsRequestTypes,
+}
 
-    #[serde(rename = "@two")]
-    two: String,
+#[derive(Debug, Deserialize, Clone)]
+pub enum PosEdmsRequestTypes {
+    AppealListRequest(#[serde(rename = "$value")] AppealListRequest),
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct AppealListRequest {
+    #[serde(rename = "ClientId")]
+    client_id: Uuid,
 }
 
 #[derive(Serialize, Clone)]
@@ -46,7 +45,7 @@ pub struct PosResponse {
 }
 
 impl Service for Pos {
-    type Request = PosRequest;
+    type Request = PosEdmsRequest;
     type Response = PosResponse;
     type Error = Infallible;
 
@@ -54,25 +53,14 @@ impl Service for Pos {
         &self,
         content: Message<Self::Request>,
     ) -> Result<Message<Self::Response>, Self::Error> {
-        tokio::time::sleep(std::time::Duration::from_millis(2500)).await;
-
         println!("Content: {:?}", content);
-
-        let mut file_path = PathBuf::new();
-        file_path.push("/tmp");
-        file_path.push(format!("{}.txt", Uuid::new_v4()));
-
-        {
-            let mut file = std::fs::File::create(&file_path).unwrap();
-            file.write(b"hello, from pos!").unwrap();
-        }
 
         Ok(Message {
             content: PosResponse {
                 response: "ALL FINE".to_string(),
                 status: 200,
             },
-            files: vec![file_path],
+            files: Vec::new(),
         })
     }
 }
