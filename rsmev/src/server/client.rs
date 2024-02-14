0aa5a6549a74ca4ba3ea137d94887c5f6302e564
Ilya <greenmine94@gmail.com>
2024-02-14 11:42:06 +0300

    feat(rsmev): remove unused result

--- rsmev/src/server/client.rs
+++ rsmev/src/server/client.rs
@@ -3,7 +3,6 @@ use std::sync::Arc;
 use super::body::Body;
 use super::handler_service::HandlerService;
 use crate::confirm_queue::{ConfirmQueue, KeyGenerator, UuidKey};
-use crate::server::Result;
 use crate::service::Service;
 
 use dashmap::DashMap;
--- rsmev/src/server/mod.rs
+++ rsmev/src/server/mod.rs
@@ -6,5 +6,3 @@ mod serve;
 mod handler_service;
 
 pub use serve::serve;
-
-type Result<S> = std::result::Result<body::Body, <S as crate::service::Service>::Error>;
